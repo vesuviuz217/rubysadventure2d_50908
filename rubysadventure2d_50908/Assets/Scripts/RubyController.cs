72df71f75bd40deaace04acb921375cf6845211f
vesuviuz217 <a50908@alunos.ipb.pt>
2023-06-23 04:51:32 +0100

    Tentativa de mudar camera

--- rubysadventure2d_50908/Assets/Scripts/RubyController.cs
+++ rubysadventure2d_50908/Assets/Scripts/RubyController.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    float horizontal;
-    float vertical;
-    Vector2 position;
-
     // Start is called before the first frame update
     void Start()
     {
-
     }
-
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        position = transform.position;
-
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
