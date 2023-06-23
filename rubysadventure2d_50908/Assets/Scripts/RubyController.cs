fd23f91b8e793e5d97bc3e151742300dd145e9a5
vesuviuz217 <a50908@alunos.ipb.pt>
2023-06-23 03:17:19 +0100

    Update RubyController.cs

--- rubysadventure2d_50908/Assets/Scripts/RubyController.cs
+++ rubysadventure2d_50908/Assets/Scripts/RubyController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    float horizontal;
+    float vertical;
+    Vector2 position;
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
-    // Update is called once per frames
+
+    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        position = transform.position;
+
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
