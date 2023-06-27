00ba5d0840496b583e88eeb1a7b347a1dba377c8
vesuviuz217 <a50908@alunos.ipb.pt>
2023-06-27 05:50:19 +0100

    Sprite Animation Completo

--- rubysadventure2d_50908/Assets/Scripts/EnemyController.cs
+++ rubysadventure2d_50908/Assets/Scripts/EnemyController.cs
@@ -56,3 +56,33 @@ public class EnemyController : MonoBehaviour
         }
     }
 }
+void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
